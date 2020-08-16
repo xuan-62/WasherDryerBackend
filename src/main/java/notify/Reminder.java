74824aa8ed77da1c7962406a911529e94801211b
bruceshenqqe <bruceshenqqe@users.noreply.github.com>
2020-08-16 12:30:05 -0400

    update reminder

---
+++ src/main/java/notify/Reminder.java
@@ -0,0 +1,43 @@
+package notify;
+
+import java.util.Date;
+
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class Reminder {
+	public void setReminder(String to, String item_id, String user_id, double time) {
+
+		to = "bruceshenqqeq@gmail.com";
+		String subject = "Reminder from Washer & Dryer Management App";
+		String text = "Your laundry in machine id: " + item_id + " will complete in 5 minutes.";
+
+		SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+		try {
+			Scheduler scheduler = schedulerFactory.getScheduler();
+			JobDetail jobDetail = JobBuilder.newJob(SendEmail.class).withIdentity(item_id, user_id).build();
+
+			jobDetail.getJobDataMap().put("to", to);
+			jobDetail.getJobDataMap().put("subject", subject);
+			jobDetail.getJobDataMap().put("text", text);
+
+			Date startDate = new Date();
+			startDate.setTime((long) (startDate.getTime() + (time * 60 * 1000)));
+
+			Trigger trigger = TriggerBuilder.newTrigger().withIdentity(item_id, user_id).startAt(startDate).build();
+
+			scheduler.scheduleJob(jobDetail, trigger);
+			System.out.println("--------scheduler start ! ------------");
+			scheduler.start();
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/main/java/rpc/Test.java
@@ -0,0 +1,55 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+
+import notify.SendEmail;
+
+/**
+ * Servlet implementation class Test
+ */
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		//JSONObject obj = new JSONObject();
+		//obj.put("username", "abcd2");
+		//RpcHelper.writeJsonObject(response, obj);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

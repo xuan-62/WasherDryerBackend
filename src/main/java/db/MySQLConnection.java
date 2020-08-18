57f77f0c4f2e9e7cfc63028ab46bdf81086e147f
bruceshenqqe <bruceshenqqe@users.noreply.github.com>
2020-08-18 18:13:41 -0400

    send email to manager, change machine status

--- src/main/java/db/MySQLConnection.java
+++ src/main/java/db/MySQLConnection.java
@@ -152,6 +152,24 @@ public class MySQLConnection {
 		}
 
 	}
+	
+	
+	public void updateCondition(String item_id, String condition) {
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return;
+		}
+		String sql = "UPDATE item SET item_condition=? WHERE item_id = ?";
+		try {
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, condition);
+			statement.setString(2, item_id);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 	//Xianli Shen
 	public void addUsertoItem(String item_id, String user_id) {
--- src/main/java/rpc/ChangeMachineStatus.java
+++ src/main/java/rpc/ChangeMachineStatus.java
@@ -5,6 +5,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import db.MySQLConnection;
 
 /**
  * Servlet implementation class ChangeMachineStatus
@@ -33,7 +39,23 @@ public class ChangeMachineStatus extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//String newStatus = request
+		
+		JSONObject input = new JSONObject(IOUtils.toString(request.getReader()));
+		String newStatus = input.getString("status");
+		String item_id = input.getString("machineId");
+		MySQLConnection connection = new MySQLConnection();
+		HttpSession session = request.getSession();
+		String user_id = session.getAttribute("user_id").toString();
+		//String user_id = "1111";
+		if(newStatus.equals("reserve")) {
+			connection.updateCondition(item_id, newStatus);
+			connection.addUsertoItem(item_id, user_id);
+			connection.setReservation(user_id, item_id);
+		}else if(newStatus.equals("available")) {
+			connection.updateCondition(item_id, newStatus);
+			connection.removeReservation(user_id, item_id);
+		}		
 	}
 
 }
